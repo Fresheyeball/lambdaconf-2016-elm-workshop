ae641ad3e5800314ca11936ec1cb3d584b458b76
Isaac Shapira <fresheyeball@gmail.com>
2016-05-12 22:56:37 -0600

    update function fleshing out

--- src/complete/Update.elm
+++ src/complete/Update.elm
@@ -5,22 +5,43 @@ import Types exposing ( Model
                       , Pet
                       , Msg(..) )
 
-import List exposing (member)
-
-add : Pet -> List LineItem -> List LineItem
-add pet ls =
-    if List.member pet <| List.map fst ls then
-        ls ++ [ (pet, 1) ]
-    else
-        ls
+import List
 
 
-no : Model -> (Model, Cmd Msg)
-no model = ( model, Cmd.none )
+noCmd : Model -> (Model, Cmd Msg)
+noCmd model = ( model, Cmd.none )
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-    case msg of
-        Add pet -> ( add pet model, Cmd.none )
-        _ -> ( model, Cmd.none )
+update msg model = let
+
+  add : Pet -> List LineItem -> List LineItem
+  add pet lineItems =
+      if List.member pet <| List.map fst lineItems then
+          lineItems ++ [ (pet, 1) ]
+      else
+          lineItems
+
+  mapWhen : (Int -> Int) -> Pet -> LineItem -> LineItem
+  mapWhen f pet (pet', volume) =
+    if pet' == pet then
+      (pet', f volume)
+    else
+      (pet', volume)
+
+  in case msg of
+    Add pet ->
+      add pet model
+      |> noCmd
+
+    Increment pet ->
+      List.map (mapWhen ((+) 1) pet) model
+      |> noCmd
+
+    Decrement pet ->
+      List.map (mapWhen ((+) 1) pet) model
+      |> noCmd
+
+    Delete pet ->
+      List.filter (\(pet', _) -> pet' == pet) model
+      |> noCmd
