34e3df52506d879401402ce53816d3557b718d5d
Isaac Shapira <fresheyeball@gmail.com>
2016-05-16 15:06:54 -0600

    docing

--- src/kit/Main.elm
+++ src/kit/Main.elm
@@ -6,6 +6,7 @@ import View
 import Update
 
 {-
+
   An Elm application consists of (atleast) 3 parts
 
   An initial `Model`,
@@ -17,7 +18,32 @@ import Update
   properties of `Signal`s. You can think of Elm like an FRP language,
   where working with `Signal`s has been made so abstract, its become opaque.
 
-  Essentially
+  Essentially you can think of it as application structured around
+  a giant fold. Consider:
+
+  folding looks something like this
+  ```
+  fold : (a -> b -> b) -> b -> f a -> b
+  ```
+
+  in Elm's case
+  ```
+  fold : (Msg -> Model -> Model) -> Model -> Signal Msg -> Signal Model
+  ```
+
+  from there its easy to get a `Signal` of `Html` for the browser to render.
+  Because of this simple model, Signals can be omitted from the langauge all
+  together as the runtime will just wire up your app like so:
+
+  runtimeRender (map view (fold update init (runtimeInputs)))
+
+  so all you have to write is the parts specific to the logic of
+  your application, namely
+
+  model : Model
+  update : Msg -> Model -> Model
+  view : Model -> Html Msg
+
 -}
 
 main : Program Never
